a35c8f5a8fe33537d1608e966462903a33391f6e
unknown <wbmeisd@centaline.com.cn>
2018-01-26 09:18:21 +0800

    echarts

---
+++ Victory.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import { VictoryBar } from "victory-native";
+
+class App extends Component {
+  render() {
+    return (
+      <VictoryBar />
+    );
+  }
+}
+
+export default App;
